a31d26de7ded156cea21bcd086a8944562b76a24
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-09-15 10:00:48 -0700

    Initial commit

---
+++ src/main/java/dev/naturecodevoid/forge/hypixelutils/HypixelUtils.java
@@ -0,0 +1,19 @@
+package dev.naturecodevoid.forge.hypixelutils;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = HypixelUtils.MODID, version = HypixelUtils.VERSION, name = HypixelUtils.NAME, clientSideOnly = true, acceptedMinecraftVersions = "@MOD_ACCEPTED@")
+public class HypixelUtils {
+    public static final String MODID = "@MOD_ID@";
+    public static final String VERSION = "@VERSION@";
+    public static final String NAME = "@MOD_NAME@";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        // some example code
+        System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
+    }
+}
