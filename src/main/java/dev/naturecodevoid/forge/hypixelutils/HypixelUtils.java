822ddd99f9c10275609a11f90dc28453bcf20049
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-09-15 17:21:38 -0700

    :zap:

--- src/main/java/dev/naturecodevoid/forge/hypixelutils/HypixelUtils.java
+++ src/main/java/dev/naturecodevoid/forge/hypixelutils/HypixelUtils.java
@@ -1,12 +1,16 @@
 package dev.naturecodevoid.forge.hypixelutils;
 
 import dev.naturecodevoid.forge.hypixelutils.listeners.CoinListener;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 @Mod(modid = HypixelUtils.MODID, version = HypixelUtils.VERSION, name = HypixelUtils.NAME, clientSideOnly = true, acceptedMinecraftVersions = "@MOD_ACCEPTED@")
 public class HypixelUtils {
@@ -15,6 +19,7 @@ public class HypixelUtils {
     public static final String NAME = "@MOD_NAME@";
     public static final String prefix = EnumChatFormatting.GREEN + "[HypixelUtils] " + EnumChatFormatting.RESET;
     public static HypixelUtilsConfig config;
+    public static GuiScreen gui = null;
     public static int totalCoins = 0;
     @Mod.Instance(HypixelUtils.MODID)
     public static HypixelUtils instance;
@@ -32,5 +37,13 @@ public class HypixelUtils {
         ClientCommandHandler.instance.registerCommand(new HypixelUtilsCommand());
 
         config = new HypixelUtilsConfig();
+    }
+
+    @SubscribeEvent
+    public void tick(TickEvent.ClientTickEvent event) {
+        if (gui != null) {
+            Minecraft.getMinecraft().displayGuiScreen(gui);
+            gui = null;
+        }
     }
 }
--- src/main/java/dev/naturecodevoid/forge/hypixelutils/HypixelUtilsCommand.java
+++ src/main/java/dev/naturecodevoid/forge/hypixelutils/HypixelUtilsCommand.java
@@ -1,14 +1,7 @@
 package dev.naturecodevoid.forge.hypixelutils;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
-import net.minecraft.util.BlockPos;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-
-import java.util.List;
 
 public class HypixelUtilsCommand extends CommandBase {
     @Override
@@ -28,14 +21,6 @@ public class HypixelUtilsCommand extends CommandBase {
 
     @Override
     public void processCommand(ICommandSender sender, String[] args) {
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @SubscribeEvent
-    public void onTick(TickEvent.ClientTickEvent e) {
-        if (e.phase == TickEvent.Phase.START) {
-            Minecraft.getMinecraft().displayGuiScreen(HypixelUtils.config.gui());
-            MinecraftForge.EVENT_BUS.unregister(this);
-        }
+        HypixelUtils.gui = HypixelUtils.config.gui();
     }
 }
