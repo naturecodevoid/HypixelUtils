ad5285c7e509d0710022066de8dd62dbadbdd5db
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2021-01-19 14:41:24 -0800

    Improve general gui

--- src/main/java/dev/naturecodevoid/forge/hypixelutils/gui/GeneralGui.java
+++ src/main/java/dev/naturecodevoid/forge/hypixelutils/gui/GeneralGui.java
@@ -3,6 +3,7 @@ package dev.naturecodevoid.forge.hypixelutils.gui;
 import dev.naturecodevoid.forge.hypixelutils.HypixelUtils;
 import dev.naturecodevoid.forge.hypixelutils.features.CoinTracker;
 import dev.naturecodevoid.forge.hypixelutils.util.DrawUtils;
+import dev.naturecodevoid.forge.hypixelutils.util.Utils;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.EnumChatFormatting;
@@ -16,28 +17,28 @@ public class GeneralGui extends GuiScreen {
                 this.height / 2 - 20 - 2,
                 150,
                 20,
-                "Open config GUI"
+                "Open GUI editor"
         ));
         this.buttonList.add(new GuiButton(2,
                 this.width / 2 - 75,
                 this.height / 2 + 2,
                 150,
                 20,
-                "Open GUI editor"
+                "Open config GUI"
         ));
         this.buttonList.add(new GuiButton(3,
                 this.width / 2 - 75,
                 this.height / 2 + 2 + 24,
                 150,
                 20,
-                "Reset coin tracker coins"
+                "Use coins from last session" // Import coin tracker coins from last session
         ));
         this.buttonList.add(new GuiButton(4,
                 this.width / 2 - 75,
                 this.height / 2 + 2 + 24 + 24,
                 150,
                 20,
-                "Use coins from last session" // Import coin tracker coins from last session
+                "Reset coin tracker coins"
         ));
     }
 
@@ -46,8 +47,8 @@ public class GeneralGui extends GuiScreen {
         if (mc.theWorld != null) this.drawGradientRect(0, 0, this.width, this.height, -1608507360, -1608507360);
         MinecraftForge.EVENT_BUS.post(new GuiScreenEvent.BackgroundDrawnEvent(this));
 
-        String text = EnumChatFormatting.WHITE + HypixelUtils.NAME + " v" + HypixelUtils.VERSION;
-        DrawUtils.drawStringWithShadow(text, (int) (((float) this.width) / 2 - ((float) DrawUtils.getStringWidth(text)) / 2), (int) (((float) this.height) / 2 - 33));
+        String text = EnumChatFormatting.GREEN + HypixelUtils.NAME + EnumChatFormatting.WHITE + " v" + HypixelUtils.VERSION;
+        DrawUtils.drawStringWithShadow(text, this.width / 2 - DrawUtils.getStringWidth(text) / 2, this.height / 2 - 33);
 
         super.drawScreen(x, y, partialTicks);
     }
@@ -55,16 +56,17 @@ public class GeneralGui extends GuiScreen {
     public void actionPerformed(GuiButton button) {
         switch (button.id) {
             case 1:
-                HypixelUtils.gui = HypixelUtils.config.gui();
-                return;
-            case 2:
                 HypixelUtils.gui = new EditorGui();
                 return;
+            case 2:
+                HypixelUtils.gui = HypixelUtils.config.gui();
+                return;
             case 3:
-                CoinTracker.coins = 0;
+                CoinTracker.coins = HypixelUtils.config.coinTrackerCoins;
                 return;
             case 4:
-                CoinTracker.coins = HypixelUtils.config.coinTrackerCoins;
+                CoinTracker.coins = 0;
+                Utils.saveConfig();
                 return;
         }
     }
